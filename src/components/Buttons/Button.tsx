f709be9d0bb2fca08c04e7d7f0d3cf42dfbe14a6
AttilaTech01 <d.hains@attila-technologies.com>
2024-09-09 15:01:49 -0400

    First iteration of Buttons completed

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,9 +1,46 @@
-export interface ButtonProps {
-  test: string;
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import classNames from "classnames";
+import "./Button.css";
+
+export enum ButtonType {
+  primary,
+  success,
+  warning,
+  danger,
 }
 
-function Button({ test }: ButtonProps) {
-  return <div>This is my button named {test}</div>;
+// Define props type for the Button component
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  disabled?: boolean;
+  outlined?: boolean;
+  rounded?: boolean;
+  buttonType?: ButtonType;
+}
+
+function Button({
+  children,
+  disabled = false,
+  outlined = false,
+  rounded = false,
+  buttonType = ButtonType.primary,
+  ...rest
+}: ButtonProps) {
+  const classes = classNames({
+    rounded: rounded,
+    primary: buttonType === ButtonType.primary,
+    success: buttonType === ButtonType.success,
+    warning: buttonType === ButtonType.warning,
+    danger: buttonType === ButtonType.danger,
+    outlined: outlined,
+    disabled: disabled,
+  });
+
+  return (
+    <button {...rest} className={classes} disabled={disabled ? true : false}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
